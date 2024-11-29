5b3eab440156566c59982932b9d165550531ab43
KelfinTDetonator <kelfinantoni@gmail.com>
2024-11-29 20:31:50 +0700

    db: init models

---
+++ app/Http/Controllers/ProductTransactionController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ProductTransaction;
+use App\Http\Requests\StoreProductTransactionRequest;
+use App\Http\Requests\UpdateProductTransactionRequest;
+
+class ProductTransactionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreProductTransactionRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ProductTransaction $productTransaction)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ProductTransaction $productTransaction)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateProductTransactionRequest $request, ProductTransaction $productTransaction)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ProductTransaction $productTransaction)
+    {
+        //
+    }
+}
