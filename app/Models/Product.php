32801dad0831bb71c7e04d304e21142d3461e1de
KelfinTDetonator <kelfinantoni@gmail.com>
2024-11-30 12:05:33 +0700

    feat(model): add category-product relation

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,11 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Product extends Model
 {
     /** @use HasFactory<\Database\Factories\ProductFactory> */
     use HasFactory;
+
+    public function categories() {
+        return $this->belongsTo(Category::class);
+    }
 }
